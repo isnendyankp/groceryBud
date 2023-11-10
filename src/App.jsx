da79abe90baa434484997a30ebf4e41ffdc5b423
isnendyankp <isnendyankp@gmail.com>
2023-11-10 14:01:17 +0700

    Create name state variable

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
 import { useState } from "react"; 
 
 const App = () => {
+  const [name, setName] = useState([]);
   return <h2>Grocery Bud - Starter</h2>;
 };
 
 export default App;
+
+// Progress
+// S7-233: Create name state variable
