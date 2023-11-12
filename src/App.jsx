5b9208c92cee7feda75407e821b8f8a21baf6e9b
isnendyankp <isnendyankp@gmail.com>
2023-11-12 12:44:04 +0700

    Create section with className="section-center"

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,13 @@ import { useState } from "react";
 
 const App = () => {
   const [name, setName] = useState([]);
-  return <h2>Grocery Bud - Starter</h2>;
+  return <section className="section-center">
+    Grocery Bud - Starter
+    </section>;
 };
 
 export default App;
 
 // Progress
 // S7-233: Create name state variable
+// S7-234: Create section with className="section-center"
