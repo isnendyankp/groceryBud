0d5aee417f556f39d4c2cbf2f5330b8ff543a7d4
isnendyankp <isnendyankp@gmail.com>
2023-11-12 12:49:35 +0700

    change div into form

--- src/Form.jsx
+++ src/Form.jsx
@@ -4,7 +4,9 @@ import { useState } from 'react'
 const Form = () => {
   const [newItemName, setNewItemName] = useState('');
   return (
-    <div>Form</div>
+    <form>
+      Form
+    </form>
   )
 }
 
@@ -14,3 +16,4 @@ export default Form
 // Progress:
 // S7-233: import { useState } from "react";
 // S7-233: create newItemName state variable
+// S7-233: change div into form
