0d4fd415971d0768e3c2092a32b03a181051cf16
isnendyankp <isnendyankp@gmail.com>
2023-11-13 13:26:37 +0700

    add div with CN form control

--- src/Form.jsx
+++ src/Form.jsx
@@ -6,6 +6,9 @@ const Form = () => {
   return (
     <form>
       <h4>grocery bud</h4>
+      <div className='form-control'>
+
+      </div>
     </form>
   )
 }
@@ -18,3 +21,4 @@ export default Form
 // S7-233: create newItemName state variable
 // S7-233: change div into form
 // S7-233: add h4 with text "grocery bud"
+// S7-233: add div with className="form-control"
