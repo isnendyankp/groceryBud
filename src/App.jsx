f30ea4ffc3a8c22374db5e9b556202fc9b6aec6f
isnendyankp <isnendyankp@gmail.com>
2023-11-14 13:01:55 +0700

    create base addItem function

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,10 @@ import Form from "./Form";
 
 const App = () => {
   const [name, setName] = useState([]);
+
+  const addItem = (itemName) => {
+    
+  }
   return <section className="section-center">
     <Form />
     </section>;
@@ -15,3 +19,4 @@ export default App;
 // S7-234: Create section with className="section-center"
 // S7-233: Pass Form component into App component
 // S7-233: import Form component
+// S7-233: create base addItem function
