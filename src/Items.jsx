f0f1623fd0ef9b97b6d58958c85e3cc604c02da2
isnendyankp <isnendyankp@gmail.com>
2023-11-15 13:16:49 +0700

    import SingleItem component

--- src/Items.jsx
+++ src/Items.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import SingleItem from './SingleItem'
 
 const Items = ({items, removeItem}) => {
   return (
@@ -11,3 +12,4 @@ export default Items
 // Progress:
 // S7-235: create Items component
 // S7-235: add items prop & removeItem prop
+// S7-235: import SingleItem component
