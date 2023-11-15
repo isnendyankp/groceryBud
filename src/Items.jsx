c9a02e4c2504dec1910f68d68173d4c3a609b7d1
isnendyankp <isnendyankp@gmail.com>
2023-11-15 13:21:36 +0700

    Add item prop to return SingleItem component

--- src/Items.jsx
+++ src/Items.jsx
@@ -5,7 +5,7 @@ const Items = ({items, removeItem}) => {
   return (
     <div className='items'>
         {items.map((item) => {
-            return <SingleItem key={item.id}/>
+            return <SingleItem key={item.id} item{item}/>
         })}
     </div>
   )
@@ -21,3 +21,4 @@ export default Items
 // S7-235: map over items array
 // S7-235: Add return SingleItem component @map method
 // S7-235: Add key prop to return SingleItem component
+// S7-235: Add item prop to return SingleItem component
