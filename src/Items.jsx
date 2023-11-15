7b7b92f2e406e62d800ea0d40690997aa0b52362
isnendyankp <isnendyankp@gmail.com>
2023-11-15 13:16:08 +0700

    add items prop & removeItem prop

--- src/Items.jsx
+++ src/Items.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const Items = () => {
+const Items = ({items, removeItem}) => {
   return (
     <div>Items</div>
   )
@@ -10,3 +10,4 @@ export default Items
 
 // Progress:
 // S7-235: create Items component
+// S7-235: add items prop & removeItem prop
