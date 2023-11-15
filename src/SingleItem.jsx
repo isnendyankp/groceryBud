cad2bf553d0278537b4e55b3fa821baefb29512b
isnendyankp <isnendyankp@gmail.com>
2023-11-15 13:02:24 +0700

    create base SingleItem component

---
+++ src/SingleItem.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const SingleItem = () => {
+  return (
+    <div>SingleItem</div>
+  )
+}
+
+export default SingleItem
+
+// Progress:
+// S7-235: create SingleItem component
