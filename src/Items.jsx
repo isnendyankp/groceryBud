09482e5490952978d2cd9200fc9e0e5605606800
isnendyankp <isnendyankp@gmail.com>
2023-11-15 13:18:57 +0700

    map over items array

--- src/Items.jsx
+++ src/Items.jsx
@@ -3,7 +3,11 @@ import SingleItem from './SingleItem'
 
 const Items = ({items, removeItem}) => {
   return (
-    <div className='items'>Items</div>
+    <div className='items'>
+        {items.map((item) => {
+            
+        })}
+    </div>
   )
 }
 
@@ -14,3 +18,4 @@ export default Items
 // S7-235: add items prop & removeItem prop
 // S7-235: import SingleItem component
 // S7-235: Add className items to div
+// S7-235: map over items array
