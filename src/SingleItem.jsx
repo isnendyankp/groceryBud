b745f411c0d77b224648231e4e0aacdd04f231bb
isnendyankp <isnendyankp@gmail.com>
2023-11-16 10:24:41 +0700

    Add input checkbox to div

--- src/SingleItem.jsx
+++ src/SingleItem.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const SingleItem = ({item, removeItem}) => {
   return (
     <div className='single-item'>
-      SingleItem
+      <input type="checkbox" />
     </div>
   )
 }
@@ -14,3 +14,4 @@ export default SingleItem
 // S7-235: create SingleItem component
 // S7-235: add item prop & removeItem prop
 // S7-235: Add className single-item to div
+// S7-235: Add input checkbox to div
