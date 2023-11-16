d4f2901f8dfb1c60c94ede2dcf823a661e68fc72
isnendyankp <isnendyankp@gmail.com>
2023-11-16 10:24:01 +0700

    Add className single-item to div

--- src/SingleItem.jsx
+++ src/SingleItem.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const SingleItem = ({item, removeItem}) => {
   return (
-    <div>SingleItem</div>
+    <div className='single-item'>
+      SingleItem
+    </div>
   )
 }
 
@@ -11,3 +13,4 @@ export default SingleItem
 // Progress:
 // S7-235: create SingleItem component
 // S7-235: add item prop & removeItem prop
+// S7-235: Add className single-item to div
